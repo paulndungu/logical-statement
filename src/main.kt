6db587823f38a7b1d740dd2871c422cbea8022bb
paulndungu <p.ndungu.kara@gmail.com>
2020-07-23 22:06:59 +0300

    added all the code

---
+++ src/main.kt
@@ -0,0 +1,22 @@
+fun main(){
+
+    println(4==3)
+    println(3==3)
+    println(5<2)
+    println(67>67)
+    println(82!=45)
+    println(82!=82)
+    println(4==3 && 3==3)
+    println(82!=45 && 2<5)
+
+    var n:Int= 20
+    println(n==20 && n>=0 && n>10)
+    println(n<30 && n<=10)
+
+    var n2:Int= 50
+    println(n2==70 || n<50)
+
+    var kotlinisntprogramming=true
+    println(!kotlinisntprogramming)
+
+}
